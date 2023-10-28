ce9564b7c9afde83a619cc14296cff73d6a32df8
myuta <myuta.dev@gmail.com>
2023-10-28 17:53:28 +0900

    dev route helper and index,show.blade.php

---
+++ resources/views/memos/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>出品メモ一覧</h1>
+    <ul>
+        <!-- リンク先をidで取得し名前で出力 -->
+        {{-- <li><a href="/memos/{{ $memos[0]->id }}">{{ $memos[0]->title }}</a></li>
+        <li><a href="/memos/{{ $memos[1]->id }}">{{ $memos[1]->title }}</a></li>
+        <li><a href="/memos/{{ $memos[2]->id }}">{{ $memos[2]->title }}</a></li> --}}
+
+
+        @foreach ($memos as $memo)
+            <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
+        @endforeach
+
+
+    </ul>
+</body>
+
+</html>
